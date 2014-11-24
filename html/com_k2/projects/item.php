0122ed4b1b3af393cef36546d416ef63335d5746
Hereward <hereward@planetonline.com.au>
2014-11-24 16:25:16 +0700

    Small change to k2 css

--- html/com_k2/projects/item.php
+++ html/com_k2/projects/item.php
@@ -284,8 +284,9 @@ defined('_JEXEC') or die;
 				<?php if($extraField->type == 'header'): ?>
 				<h4 class="itemExtraFieldsHeader"><?php echo $extraField->name; ?></h4>
 				<?php else: ?>
-				<span class="itemExtraFieldsLabel"><?php echo $extraField->name; ?>:</span>
-				<span class="itemExtraFieldsValue"><?php echo $extraField->value; ?></span>
+				<!-- <span class="itemExtraFieldsLabel"><?php echo $extraField->name; ?>:</span> -->
+                                
+				<span class="itemExtraFieldsValue"><?php echo $extraField->name; ?>: <?php echo $extraField->value; ?></span>
 				<?php endif; ?>
 			</li>
 			<?php endif; ?>
